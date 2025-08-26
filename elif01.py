8fd43f6bad6300dd05e69e651fae96a9a92b647a
baxtiyorovbekzod <bekzodbaxtiyorov142@gmail.com>
2025-08-26 21:22:02 +0500

    ok2

--- elif01.py
+++ elif01.py
@@ -1,2 +1,13 @@
-
-rt5y5y5y5y
+bal=int(input("ball:"))
+if 90<= bal <=100:
+    print("A (A'lo)")
+elif 80<= bal<=89:
+    print("B (Yaxshi)")
+elif 70<= bal <= 79:
+    print("C (Qoniqarli)")
+elif 60<= bal <=69:
+    print("D (Qoniqarsiz)")
+elif 0<= bal<= 59:
+    print("F (Rad)")
+else:
+    print("bal xato kiritildi")            
