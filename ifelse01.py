44406fc55c2d62cdcdbcc0b793d414d1d977d60f
baxtiyorovbekzod <bekzodbaxtiyorov142@gmail.com>
2025-08-27 00:18:36 +0500

    ok

--- ifelse01.py
+++ ifelse01.py
@@ -1,1 +1,5 @@
-
+yosh=int(input("yoshizni kiriting:"))
+if yosh>=18:
+    print("Kirishga ruxsat berildi.")
+else:
+    print("Kirish rad etildi.")
--- ifelse04.py
+++ ifelse04.py
@@ -1,1 +1,6 @@
-
+pul=5000
+yechmoqchi_sum=int(input("Summani kiriting:"))
+if pul- yechmoqchi_sum >=0:
+    print(f"Pul yechildi. Qolgan balans: {pul-yechmoqchi_sum} so'm")
+else:
+    print("Mablag' yetarli emas. Sizning balansingiz: 5000 so'm")    
